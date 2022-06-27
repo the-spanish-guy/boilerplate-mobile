1c86d1ee385b9a33356e4cfcc2598724643bd812
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-06-26 21:52:41 -0300

    :sparkles: feat: add config to custom paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [[
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@app': './src',
+        },
+      },
+    ],'react-native-reanimated/plugin'],
   };
 };
