e1ef1f4230e21c6c5e5d79c7b1f60c4f87d7fa7e
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-06-26 22:28:35 -0300

    :sparkles: feat: add theme config

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import Routes from '@app/routes'
+import { ThemeProvider } from '@shopify/restyle'
+import light from '@app/themes/Light'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
+      <ThemeProvider theme={light}>
+        <Routes />
+      </ThemeProvider>
     </NavigationContainer>
   )
 }
---
+++ src/themes/Light.ts
@@ -0,0 +1,73 @@
+import { createTheme } from '@shopify/restyle'
+import { StatusBarStyle } from 'expo-status-bar'
+
+const palette = {
+  purpleLight: '#8C6FF7',
+  purplePrimary: '#5A31F4',
+  purpleDark: '#3F22AB',
+
+  greenLight: '#56DCBA',
+  greenPrimary: '#0ECD9D',
+  greenDark: '#0A906E',
+
+  black: '#0B0B0B',
+  white: '#F0F2F3',
+
+  red: 'red',
+  blue: 'blue',
+  yellow: 'yellow',
+  paper00: '#ffffff',
+  paper10: '#f5f5f4',
+  paper20: '#e6e6e6',
+  paper300: '#767577',
+  paper900: '#202020',
+  blue70: '#2185d0',
+  navy20: '#171a21',
+  navy900: '#b9babc'
+}
+
+const theme = createTheme({
+  spacing: {
+    '0': 0,
+    xs: 4,
+    sm: 8,
+    md: 12,
+    lg: 16,
+    xl: 24,
+    xxl: 48,
+    hg: 128
+  },
+  breakpoints: {
+    phone: 0,
+    tablet: 768
+  },
+  colors: {
+    white: palette.white,
+    black: palette.black,
+    red: palette.red,
+    blue: palette.blue,
+    yellow: palette.yellow,
+
+    $primary: palette.blue70,
+    $windowBackground: '#f0f0f0',
+    $background: palette.paper10,
+    $foreground: palette.paper900,
+    $sidebarBackground: palette.navy20,
+    $sidebarForeground: palette.navy900,
+    $sidebarSeparator: palette.paper00 + '20',
+    $headerBarBackground: palette.paper20
+  },
+
+  statusBar: {
+    barStyle: 'dark-content' as StatusBarStyle
+  },
+  textVariants: {
+    defaults: {
+      color: '$foreground',
+      fontSize: 16
+    }
+  }
+})
+
+export default theme
+export type Theme = typeof theme
