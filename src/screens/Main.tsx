117a764695104d3f62bbeae71c8f73e6dc1d842b
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-06-27 22:13:03 -0300

    :sparkles: feat(screen): add main screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,10 +1,37 @@
-import { Box, Text } from '@app/atoms'
+import { useCallback } from 'react'
+import { DrawerScreenProps } from '@react-navigation/drawer'
+import { CompositeScreenProps } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-const MainScreen = () => {
+import FeatherIcon from '@app/components/Icon'
+import { Box, Container, Text } from '@app/atoms'
+import { HomeDrawerParamList, RootStackParamList } from '@app/routes'
+
+type Props = CompositeScreenProps<
+  DrawerScreenProps<HomeDrawerParamList, 'Main'>,
+  NativeStackScreenProps<RootStackParamList>
+>
+
+const MainScreen = ({ navigation }: Props) => {
+  const handleSideBar = useCallback(() => {
+    navigation.toggleDrawer()
+  }, [navigation])
+
   return (
-    <Box flex={1} justifyContent="center" alignItems="center">
-      <Text>Hello World!!!</Text>
-    </Box>
+    <Container>
+      <Box mt="xxl" flexDirection="row" alignItems="center" m="xl">
+        <TouchableOpacity onPress={handleSideBar}>
+          <FeatherIcon name="menu" size={22} />
+        </TouchableOpacity>
+        <Box flex={1} alignItems="center">
+          <Text variant="title">Boilerplate</Text>
+        </Box>
+      </Box>
+      <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Hello World!!!</Text>
+      </Box>
+    </Container>
   )
 }
 
