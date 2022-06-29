fc01ca15ccb8609a4c829e41bc4bbab59ccd18a7
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-06-28 21:16:03 -0300

    :construction: feat(component): wip sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,11 @@
 import { Box, Text } from '@app/atoms'
 import { DrawerContentComponentProps } from '@react-navigation/drawer'
+import { TouchableHighlight } from 'react-native-gesture-handler'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
-const Sidebar: React.FC<DrawerContentComponentProps> = () => {
+// const menuItems = {}
+
+const Sidebar: React.FC<DrawerContentComponentProps> = ({ ...props }) => {
   return (
     <SafeAreaProvider>
       <Box flex={1} bg="$sidebarBackground">
@@ -18,6 +21,20 @@ const Sidebar: React.FC<DrawerContentComponentProps> = () => {
             <Text variant="sidebar" m="lg">
               Boilerplate
             </Text>
+          </Box>
+          <Box alignItems="flex-start" p="lg" borderWidth={1}>
+            <TouchableHighlight
+              onPress={() => props.navigation.navigate('Main')}
+            >
+              <Text variant="sidebar">Main Page</Text>
+            </TouchableHighlight>
+          </Box>
+          <Box alignItems="flex-start" p="lg" borderWidth={1}>
+            <TouchableHighlight
+              onPress={() => props.navigation.navigate('Detail')}
+            >
+              <Text variant="sidebar">Details Page</Text>
+            </TouchableHighlight>
           </Box>
         </SafeAreaView>
       </Box>
