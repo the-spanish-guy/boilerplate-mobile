4bf91d6c26d0cd715399a3301a50e929dfe930b0
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-06-28 21:14:34 -0300

    :recycle: feat(screen): small refactor in main screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,11 +1,9 @@
-import { useCallback } from 'react'
 import { DrawerScreenProps } from '@react-navigation/drawer'
 import { CompositeScreenProps } from '@react-navigation/native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-import FeatherIcon from '@app/components/Icon'
 import { Box, Container, Text } from '@app/atoms'
+import HeaderBar from '@app/components/HeaderBar'
 import { HomeDrawerParamList, RootStackParamList } from '@app/routes'
 
 type Props = CompositeScreenProps<
@@ -13,22 +11,17 @@ type Props = CompositeScreenProps<
   NativeStackScreenProps<RootStackParamList>
 >
 
-const MainScreen = ({ navigation }: Props) => {
-  const handleSideBar = useCallback(() => {
-    navigation.toggleDrawer()
-  }, [navigation])
-
+const MainScreen = ({ ...props }: Props) => {
   return (
     <Container>
-      <Box mt="xxl" flexDirection="row" alignItems="center" m="xl">
-        <TouchableOpacity onPress={handleSideBar}>
-          <FeatherIcon name="menu" size={22} />
-        </TouchableOpacity>
-        <Box flex={1} alignItems="center">
-          <Text variant="title">Boilerplate</Text>
-        </Box>
-      </Box>
-      <Box flex={1} justifyContent="center" alignItems="center">
+      <HeaderBar {...props} />
+      <Box
+        flex={1}
+        justifyContent="center"
+        alignItems="center"
+        borderWidth={1}
+        borderColor="red"
+      >
         <Text>Hello World!!!</Text>
       </Box>
     </Container>
