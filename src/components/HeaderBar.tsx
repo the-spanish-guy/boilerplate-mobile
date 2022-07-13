e127096bbdac9892e38108f56933067ed2d36c7d
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-07-13 16:54:59 -0300

    :sparkles: feat(component): show theme picker

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -2,26 +2,45 @@ import { useCallback } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { DrawerScreenProps } from '@react-navigation/drawer'
 
-import { Box, Text } from '@app/atoms'
+import { Box, Text, Button } from '@app/atoms'
 import { HomeDrawerParamList } from '@app/routes'
 
 import FeatherIcon from './Icon'
 
-const HeaderBar: React.FC<DrawerScreenProps<HomeDrawerParamList>> = ({
-  navigation
+type Props = {
+  handleThemePicker?: () => void
+}
+
+const HeaderBar: React.FC<DrawerScreenProps<HomeDrawerParamList> & Props> = ({
+  navigation,
+  handleThemePicker
 }) => {
   const handleSideBar = useCallback(() => {
     navigation.toggleDrawer()
   }, [navigation])
 
   return (
-    <Box mt="xxl" flexDirection="row" alignItems="center" m="xl">
+    <Box
+      mt="xxl"
+      flexDirection="row"
+      alignItems="center"
+      m="xl"
+      justifyContent="space-between"
+    >
       <TouchableOpacity onPress={handleSideBar}>
         <FeatherIcon name="menu" size={22} />
       </TouchableOpacity>
-      <Box flex={1} alignItems="center">
-        <Text variant="title">Boilerplate</Text>
-      </Box>
+      <Text variant="title">Boilerplate</Text>
+
+      {handleThemePicker ? (
+        <Button onPress={handleThemePicker}>
+          <FeatherIcon name="more-vertical" size={22} />
+        </Button>
+      ) : (
+        <>
+          <Text>{''}</Text>
+        </>
+      )}
     </Box>
   )
 }
