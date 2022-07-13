0a4c714c1d1c3dc8d7df531694e2c62db0d94f18
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-07-13 12:03:07 -0300

    :sparkles: feat: centering text

--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,9 +1,10 @@
-import { Container, Text } from '@app/atoms'
-import HeaderBar from '@app/components/HeaderBar'
-import { HomeDrawerParamList, RootStackParamList } from '@app/routes'
 import { DrawerScreenProps } from '@react-navigation/drawer'
 import { CompositeScreenProps } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import { Box, Container, Text } from '@app/atoms'
+import HeaderBar from '@app/components/HeaderBar'
+import { HomeDrawerParamList, RootStackParamList } from '@app/routes'
 
 type Props = CompositeScreenProps<
   DrawerScreenProps<HomeDrawerParamList, 'Detail'>,
@@ -14,7 +15,9 @@ const DetailScreen = ({ ...props }: Props) => {
   return (
     <Container>
       <HeaderBar {...props} />
-      <Text>Details Screen</Text>
+      <Box flex={1} justifyContent="center" alignItems="center">
+        <Text>Details Screen</Text>
+      </Box>
     </Container>
   )
 }
